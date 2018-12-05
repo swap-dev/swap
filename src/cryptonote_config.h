0a9ffaf6f44e5c4ca689bbd7260ca371573b7db8
sebseb7 <sebgreenbus@gmail.com>
2018-12-05 06:50:24 +0100

    adjust ringsize

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -131,8 +131,6 @@
 
 #define THREAD_STACK_SIZE                       5 * 1024 * 1024
 
-#define HF_VERSION_MIN_MIXIN_4                  6
-#define HF_VERSION_MIN_MIXIN_6                  7
 #define HF_VERSION_MIN_MIXIN_10                 8
 #define HF_VERSION_PER_BYTE_FEE                 8
 #define HF_VERSION_SMALLER_BP                   10
