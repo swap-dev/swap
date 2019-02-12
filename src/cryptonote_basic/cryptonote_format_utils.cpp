140579aca33429dec096a3f3f9426a188e7fcb5f
sebseb7 <sebgreenbus@gmail.com>
2019-02-12 06:19:23 +0100

    thread fix for windows

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -38,9 +38,7 @@
 #include "crypto/crypto.h"
 #include "crypto/hash.h"
 #include "ringct/rctSigs.h"
-extern "C" {
 #include "crypto/cuckaroo/cuckaroo29s.h"
-}
 
 using namespace epee;
 
@@ -1281,7 +1279,8 @@ namespace cryptonote
 		edges[30]=b.cycle31;
 		edges[31]=b.cycle32;
 
-		cuckaroo29s(bd.data(), bd.size(),b.nonce, edges, res.data);
+		Cuckaroo29S* cu = new Cuckaroo29S();
+		cu->hash(bd.data(), bd.size(),b.nonce, edges, res.data);
 	}
 	else
 	{
