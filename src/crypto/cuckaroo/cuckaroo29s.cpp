17b94066cb47e65ecea970281f69681c6fb2cd28
sebseb7 <sebgreenbus@gmail.com>
2019-02-12 06:19:23 +0100

    thread fix for windows

--- src/crypto/cuckaroo/cuckaroo29s.cpp
+++ src/crypto/cuckaroo/cuckaroo29s.cpp
@@ -6,22 +6,15 @@
 #include "int-util.h"
 #include "cuckaroo29s.h"
 
-typedef struct siphash_keys__
-{
-	uint64_t k0;
-	uint64_t k1;
-	uint64_t k2;
-	uint64_t k3;
-} siphash_keys;
 
-static void setsipkeys(const char *keybuf,siphash_keys *keys) {
+void Cuckaroo29S::setsipkeys(const unsigned char *keybuf,siphash_keys *keys) {
 	keys->k0 = htole64(((uint64_t *)keybuf)[0]);
 	keys->k1 = htole64(((uint64_t *)keybuf)[1]);
 	keys->k2 = htole64(((uint64_t *)keybuf)[2]);
 	keys->k3 = htole64(((uint64_t *)keybuf)[3]);
 }
-static void setheader(const char *header, const uint32_t headerlen, siphash_keys *keys) {
-	char hdrkey[32];
+void Cuckaroo29S::setheader(const unsigned char *header, const uint32_t headerlen, siphash_keys *keys) {
+	unsigned char hdrkey[32];
 	blake2b((void *)hdrkey, sizeof(hdrkey), (const void *)header, headerlen, 0, 0);
 	setsipkeys(hdrkey,keys);
 }
@@ -35,31 +28,27 @@ static void setheader(const char *header, const uint32_t headerlen, siphash_keys
 #define EDGE_BLOCK_MASK (EDGE_BLOCK_SIZE - 1)
 #define NEDGES ((uint32_t)1 << EDGEBITS)
 #define EDGEMASK ((uint32_t)NEDGES - 1)
-static uint64_t v0;
-static uint64_t v1;
-static uint64_t v2;
-static uint64_t v3;
-static uint64_t rotl(uint64_t x, uint64_t b) {
+uint64_t  Cuckaroo29S::rotl(uint64_t x, uint64_t b) {
 	return (x << b) | (x >> (64 - b));
 }
-static void sip_round(void) {
+void Cuckaroo29S::sip_round(void) {
 	v0 += v1; v2 += v3; v1 = rotl(v1,13);
 	v3 = rotl(v3,16); v1 ^= v0; v3 ^= v2;
 	v0 = rotl(v0,32); v2 += v1; v0 += v3;
 	v1 = rotl(v1,17);   v3 = rotl(v3,21);
 	v1 ^= v2; v3 ^= v0; v2 = rotl(v2,32);
 }
-static void hash24(const uint64_t nonce) {
+void  Cuckaroo29S::hash24(const uint64_t nonce) {
 	v3 ^= nonce;
 	sip_round(); sip_round();
 	v0 ^= nonce;
 	v2 ^= 0xff;
 	sip_round(); sip_round(); sip_round(); sip_round();
 }
-static uint64_t xor_lanes(void) {
+uint64_t Cuckaroo29S::xor_lanes(void) {
 	return (v0 ^ v1) ^ (v2  ^ v3);
 }
-static uint64_t sipblock(siphash_keys *keys, const uint32_t edge,uint64_t  *buf) {
+uint64_t Cuckaroo29S::sipblock(siphash_keys *keys, const uint32_t edge,uint64_t  *buf) {
 	v0=keys->k0;
 	v1=keys->k1;
 	v2=keys->k2;
@@ -76,7 +65,7 @@ static uint64_t sipblock(siphash_keys *keys, const uint32_t edge,uint64_t  *buf)
 	return buf[edge & EDGE_BLOCK_MASK];
 }
 enum verify_code { POW_OK, POW_HEADER_LENGTH, POW_TOO_BIG, POW_TOO_SMALL, POW_NON_MATCHING, POW_BRANCH, POW_DEAD_END, POW_SHORT_CYCLE};
-static int verify(uint32_t edges[PROOFSIZE], siphash_keys *keys) {
+int Cuckaroo29S::verify(uint32_t edges[PROOFSIZE], siphash_keys *keys) {
 	uint32_t xor0 = 0, xor1 = 0;
 	uint64_t sips[EDGE_BLOCK_SIZE];
 	uint32_t uvs[2*PROOFSIZE];
@@ -107,27 +96,30 @@ static int verify(uint32_t edges[PROOFSIZE], siphash_keys *keys) {
 	} while (i != 0);           // must cycle back to start or we would have found branch
 	return n == PROOFSIZE ? POW_OK : POW_SHORT_CYCLE;
 }
-const char *errstr[] = { "OK", "wrong header length", "edge too big", "edges not ascending", "endpoints don't match up", "branch in cycle", "cycle dead ends", "cycle too short"};
 
-int cuckaroo29s(const void* in, size_t len, uint32_t nonce, uint32_t *edges,void* out)
+
+Cuckaroo29S::Cuckaroo29S(){
+}
+
+int Cuckaroo29S::hash(const void* in, size_t len, uint32_t nonce, uint32_t *edges,void* out)
 {
 	
 	siphash_keys keys;
-	char header[255];
+	unsigned char header[255];
 
 	for(uint32_t i = 0;i < len;i++)
-		header[i]=((char*)in)[i];
+		header[i]=((unsigned char*)in)[i];
 
 	header[len+3] = nonce & 0xff;
 	header[len+2] = (nonce >> 8 ) & 0xff ;
 	header[len+1] = (nonce >> 16 ) & 0xff ;
 	header[len]   = (nonce >> 24 ) & 0xff ;
-
-	setheader((const char*)header,len+4,&keys);
 	
-	int retval = verify(edges,&keys);
-
-	char* out2 = out;
+	setheader((const unsigned char*)header,len+4,&keys);
+	
+	int retval = this->verify(edges,&keys);
+	
+	unsigned char* out2 = (unsigned char*)out;
 	
 	if (retval == 0)
 	{
@@ -152,7 +144,11 @@ int cuckaroo29s(const void* in, size_t len, uint32_t nonce, uint32_t *edges,void
 		}
 	
 		unsigned char cyclehash[32];
-		blake2b((void *)cyclehash, sizeof(cyclehash), (uint8_t *)hashdata, sizeof(hashdata), 0, 0);
+
+		blake2b_state S[1];
+		blake2b_init( S, 32 );
+		blake2b_update( S, ( const uint8_t * )hashdata, 116 );
+		blake2b_final( S, cyclehash, 32 );
 	
 		for(int i = 0; i < 32; i++)
 			out2[i] = cyclehash[31-i];
@@ -162,5 +158,4 @@ int cuckaroo29s(const void* in, size_t len, uint32_t nonce, uint32_t *edges,void
 			out2[i]=0xff;
 	
 	return retval;
-}
-
+};
