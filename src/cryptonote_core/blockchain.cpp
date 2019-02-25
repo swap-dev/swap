6fb05208bfa09335bd3c562c26437a6dc2614b4a
sebseb7 <sebgreenbus@gmail.com>
2019-02-25 19:07:01 +0100

    fix pruning activation fork

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -3610,9 +3610,9 @@ leave:
 bool Blockchain::prune_blockchain(uint32_t pruning_seed)
 {
   uint8_t hf_version = m_hardfork->get_current_version();
-  if (hf_version < 10)
+  if (hf_version < HF_VERSION_SMALLER_BP)
   {
-    MERROR("Most of the network will only be ready for pruned blockchains from v10, not pruning");
+    MERROR("Most of the network will only be ready for pruned blockchains from v" << HF_VERSION_SMALLER_BP << ", not pruning");
     return false;
   }
   return m_db->prune_blockchain(pruning_seed);
