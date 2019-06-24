e4c13031bcfcff897e626320485546e898b82f3c
LPHuynh <30508709+LPHuynh@users.noreply.github.com>
2019-06-24 14:40:07 +0000

    rebrand

--- src/daemon/command_server.cpp
+++ src/daemon/command_server.cpp
@@ -217,12 +217,12 @@ t_command_server::t_command_server(
     m_command_lookup.set_handler(
       "start_save_graph"
     , std::bind(&t_command_parser_executor::start_save_graph, &m_parser, p::_1)
-    , "Start saving data for dr monero."
+    , "Start saving data for dr swap."
     );
     m_command_lookup.set_handler(
       "stop_save_graph"
     , std::bind(&t_command_parser_executor::stop_save_graph, &m_parser, p::_1)
-    , "Stop saving data for dr monero."
+    , "Stop saving data for dr swap."
     );
     m_command_lookup.set_handler(
       "hard_fork_info"
