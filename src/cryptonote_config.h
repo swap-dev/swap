1887608136c7b85cb695444e0ee99edb61d7c0a7
LPHuynh <30508709+LPHuynh@users.noreply.github.com>
2019-06-28 16:44:53 +0000

    increase default sync speed limit to 1Gbps

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -113,7 +113,7 @@
 #define P2P_DEFAULT_ANCHOR_CONNECTIONS_COUNT            8
 #define P2P_DEFAULT_SYNC_SEARCH_CONNECTIONS_COUNT       2
 #define P2P_DEFAULT_LIMIT_RATE_UP                       2048       // kB/s
-#define P2P_DEFAULT_LIMIT_RATE_DOWN                     8192       // kB/s
+#define P2P_DEFAULT_LIMIT_RATE_DOWN                     131072     // kB/s
 
 #define P2P_FAILED_ADDR_FORGET_SECONDS                  (60*60)     //1 hour
 #define P2P_IP_BLOCKTIME                                (60*60*24)  //24 hour
