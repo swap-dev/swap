cb3b13956cdc63aab60343420044e1518281de7c
reeyon <reeyonlim@gmail.com>
2019-10-11 09:34:11 +0800

    Up to date checkpoints.dat

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -4897,7 +4897,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "124952fbc4315f6d084192f203ff695392496b6859e2b8bb92e72231a933e478";
+static const char expected_block_hashes_hash[] = "513fd7531cf813fa7108546882bb070bf494f81c924266cded3abc3cc9d74825";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
