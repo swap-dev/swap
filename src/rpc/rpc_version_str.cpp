eef4b1baeedbf91553cc2702fcbef12a1809c7b4
LPHuynh <30508709+LPHuynh@users.noreply.github.com>
2020-07-29 12:16:13 -0400

    fix version display

--- src/rpc/rpc_version_str.cpp
+++ src/rpc/rpc_version_str.cpp
@@ -36,18 +36,9 @@ namespace cryptonote
 namespace rpc
 {
 
-// Expected format of Monero software version string:
-// 1) Four numbers, one to two digits each, separated by periods
-// 2) Optionally, one of the following suffixes:
-//      a) -release
-//      b) -<hash> where <hash> is exactly nine lowercase hex digits
-
 bool is_version_string_valid(const std::string& str)
 {
-    return std::regex_match(str, std::regex(
-        "^\\d{1,2}(\\.\\d{1,2}){3}(-(release|[0-9a-f]{9}))?$",
-        std::regex_constants::nosubs
-    ));
+    return true; //let's not complicate things for no reasons...
 }
 
 }  // namespace rpc
